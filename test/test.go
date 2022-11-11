91e1a580896a5107be510343f20c575b9221548b
michelesalvv <michelesalvv@gmail.com>
2022-11-11 18:05:22 +0100

    added single test mode

--- test/test.go
+++ test/test.go
@@ -9,141 +9,85 @@ import (
 	"os"
 	"strconv"
 	"strings"*/
-	"encoding/csv"
+
 	"fmt"
 	"kmeans-MR/utils"
 	"log"
 	"net/rpc"
 	"os"
-	"path/filepath"
-	"regexp"
 	"strconv"
-	"strings"
 )
 
-const DATASET_DIR string = "datasets/"
-
-type Dataset struct {
-	Path      string
-	Name      string
-	Instances int
-	K         int
-}
+var client *rpc.Client
+var err error
 
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("Please specify service [address:port] and [numerOfMappers] to perform tests")
+	if len(os.Args) < 3 {
+		fmt.Println("Please specify service [address:port] [MULTIPLE/ALLDATA] [numerOfMappers] optional{[datasetName], [RUNS NUMBER] to perform tests")
 		os.Exit(1)
 	}
-	addr := os.Args[1]
 
-	client, err := rpc.Dial("tcp", addr)
+	// Connecting to service
+	addr := os.Args[1]
+	MAPPER_NUMS = os.Args[3]
+	client, err = rpc.Dial("tcp", addr)
 	if err != nil {
 		log.Fatal("Error in dialing: ", err)
 	}
 	defer client.Close()
 
-	var datasets []Dataset
-	datasets, fileName := fetchDatasets()
+	switch os.Args[2] {
+	case MODE_ALL_DATASET:
+		run_test(os.Args[4], MODE_ALL_DATASET, 1)
 
-	for i, dataset := range datasets {
-		log.Printf("Test #%d on %s", i, dataset.Name)
-		kmeansInput := utils.InputKMeans{Dataset: dataset.Name, Clusters: dataset.K}
-		var finalResult utils.Result
-
-		err = client.Call("Master.KMeans", kmeansInput, &finalResult)
-		if err != nil {
-			log.Fatal("Error in Master.KMeans: \n", err.Error())
+	case MODE_MULTIPLE:
+		var runs int
+		if len(os.Args) > 4 {
+			if runs, _ = strconv.Atoi(os.Args[5]); runs > 0 {
+				PrintResults(run_test(os.Args[4], MODE_MULTIPLE, runs))
+			}
+			break
 		}
-		if saveBenchmark(finalResult, dataset, fileName) {
-			log.Print("Test Done ...")
-			saveResults(finalResult, dataset.Name)
+		log.Fatal("Please use a valid run test numbers!")
+
+	default:
+		log.Fatal("Please select a valid test mode [MULTIPLE/ALLDATA]")
+	}
+
+	// var datasets []Dataset
+	// datasets, fileName := fetchDatasets()
+
+	// for i, dataset := range datasets {
+	// 	log.Printf("Test #%d on %s", i, dataset.Name)
+	// 	kmeansInput := utils.InputKMeans{Dataset: dataset.Name, Clusters: dataset.K}
+	// 	var finalResult utils.Result
+
+	// 	err = client.Call("Master.KMeans", kmeansInput, &finalResult)
+	// 	if err != nil {
+	// 		log.Fatal("Error in Master.KMeans: \n", err.Error())
+	// 	}
+	// 	if saveBenchmark(finalResult, dataset, fileName) {
+	// 		log.Print("Test Done ...")
+	// 		saveResults(finalResult, dataset.Name)
+	// 	}
+	// }
+}
+
+func run_test(datasetName, testingMode string, runs int) []utils.Result {
+
+	dataset := FetchSingleDataset(datasetName)
+	var results []utils.Result
+
+	switch testingMode {
+
+	case MODE_MULTIPLE:
+		log.Printf("Testing on %s", dataset.Name)
+		for i := 0; i < runs; i++ {
+			kmeansInput := utils.InputKMeans{Dataset: dataset.Name, Clusters: dataset.K}
+			results = append(results, KMeans(kmeansInput))
 		}
 	}
-}
 
-// dataset 3d 2cluster 1000samples format
-func fetchDatasets() ([]Dataset, string) {
-	log.Print("Fetching Datasets ...")
-	ex, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	exPath := filepath.Dir(ex)
-	datasetsPath := strings.Replace(exPath, "test", DATASET_DIR, 1)
-
-	files, err := os.ReadDir(datasetsPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var fileName string = os.Args[2] + "_mapper_" + utils.TEST_FILE
-	TouchFile(fileName)
-
-	var datasets []Dataset
-	for _, dir := range files {
-		tokens := strings.Split(dir.Name(), "_")
-		re := regexp.MustCompile("[0-9]+")
-		if dir.IsDir() && strings.Contains(tokens[0], "dataset") {
-			k, _ := strconv.Atoi(re.FindString(tokens[2]))
-			inst, _ := strconv.Atoi(re.FindString(tokens[3]))
-			datasets = append(datasets, Dataset{Path: DATASET_DIR + dir.Name(), K: k, Instances: inst, Name: dir.Name() + ".csv"})
-		}
-	}
-
-	return datasets, fileName
-}
-
-func saveBenchmark(res utils.Result, dataset Dataset, fileName string) bool {
-
-	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal("Error: ", err)
-	}
-	defer f.Close()
-
-	csvwriter := csv.NewWriter(f)
-	defer csvwriter.Flush()
-
-	var line []string
-	executionTime := res.ExecutionTime.Milliseconds()
-	line = append(line, strconv.Itoa(dataset.Instances), strconv.Itoa(dataset.K), strconv.Itoa(res.Iterations), strconv.Itoa(int(executionTime)))
-	csvwriter.Write(line)
-	return true
-}
-
-func saveResults(res utils.Result, datasetName string) bool {
-	filename := strings.Replace(datasetName, "dataset", "centroids", 1)
-
-	csvFile, err := os.Create(filename)
-	if err != nil {
-		log.Print("Failed creating file: ", err)
-		return false
-	}
-	defer csvFile.Close()
-
-	csvwriter := csv.NewWriter(csvFile)
-	for _, point := range res.Centroids {
-		line := strings.Fields(strings.Trim(fmt.Sprint(point.Values), "[]"))
-		csvwriter.Write(line)
-	}
-	csvwriter.Flush()
-	return true
-}
-
-func TouchFile(name string) os.File {
-	file, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Fatal("File creation error")
-	}
-	defer file.Close()
-
-	csvwriter := csv.NewWriter(file)
-
-	var header []string
-	header = append(header, "Points", "Clusters", "Iterations", "ExecutionTime")
-	csvwriter.Write(header)
-	csvwriter.Flush()
-	return *file
+	return results
 }
---
+++ test/utilities.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"kmeans-MR/utils"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+const TEST_FILE_FORMAT = "_mapper_results.csv"
+
+type Dataset struct {
+	Path      string
+	Name      string
+	Instances int
+	K         int
+}
+
+const DATASET_DIR string = "datasets"
+const MODE_ALL_DATASET = "ALLDATA"
+const MODE_MULTIPLE = "MULTIPLE"
+
+var MAPPER_NUMS string
+
+func KMeans(kmeansInput utils.InputKMeans) utils.Result {
+	var finalResult utils.Result
+
+	err := client.Call("Master.KMeans", kmeansInput, &finalResult)
+	if err != nil {
+		log.Fatal("Error in Master.KMeans: \n", err.Error())
+	}
+	return finalResult
+}
+
+// dataset 3d 2cluster 1000samples format
+func fetchDatasets() ([]Dataset, string) {
+	log.Print("Fetching Datasets ...")
+	ex, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exPath := filepath.Dir(ex)
+	datasetsPath := strings.Replace(exPath, "test", DATASET_DIR, 1)
+
+	files, err := os.ReadDir(datasetsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var fileName string = os.Args[2] + "_mapper_" + utils.TEST_FILE
+	TouchFile(fileName)
+
+	var datasets []Dataset
+	for _, dir := range files {
+		tokens := strings.Split(dir.Name(), "_")
+		re := regexp.MustCompile("[0-9]+")
+		if dir.IsDir() && strings.Contains(tokens[0], "dataset") {
+			k, _ := strconv.Atoi(re.FindString(tokens[2]))
+			inst, _ := strconv.Atoi(re.FindString(tokens[3]))
+			datasets = append(datasets, Dataset{Path: DATASET_DIR + dir.Name(), K: k, Instances: inst, Name: dir.Name() + ".csv"})
+		}
+	}
+
+	return datasets, fileName
+}
+
+// dataset 3d 2cluster 1000samples format
+func FetchSingleDataset(datasetName string) Dataset {
+	datasetName = strings.Split(datasetName, ".")[0]
+	ex, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exPath := filepath.Dir(ex)
+	datasetPath := fmt.Sprintf("%s/%s/%s.csv", strings.Replace(exPath, "test", DATASET_DIR, 1), datasetName, datasetName)
+
+	//check if file exists and takes metadata
+	var dataset Dataset
+	re := regexp.MustCompile("[0-9]+")
+	if _, err := os.Stat(datasetPath); err == nil {
+		tokens := strings.Split(datasetName, "_")
+		k, _ := strconv.Atoi(re.FindString(tokens[2]))
+		inst, _ := strconv.Atoi(re.FindString(tokens[3]))
+		dataset = Dataset{Path: datasetPath, K: k, Instances: inst, Name: datasetName + ".csv"}
+	} else {
+		log.Fatal("Please use a valid dataset name. Include .csv extension!")
+	}
+
+	return dataset
+}
+
+func TouchFile(name string) os.File {
+	file, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal("File creation error")
+	}
+	defer file.Close()
+
+	csvwriter := csv.NewWriter(file)
+
+	var header []string
+	header = append(header, "Points", "Clusters", "Iterations", "ExecutionTime")
+	csvwriter.Write(header)
+	csvwriter.Flush()
+	return *file
+}
+
+func SaveBenchmark(res utils.Result, dataset Dataset, fileName string) bool {
+
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	defer f.Close()
+
+	csvwriter := csv.NewWriter(f)
+	defer csvwriter.Flush()
+
+	var line []string
+	executionTime := res.ExecutionTime.Milliseconds()
+	line = append(line, strconv.Itoa(dataset.Instances), strconv.Itoa(dataset.K), strconv.Itoa(res.Iterations), strconv.Itoa(int(executionTime)))
+	csvwriter.Write(line)
+	return true
+}
+
+func SaveResults(res utils.Result, datasetName string) bool {
+	filename := strings.Replace(datasetName, "dataset", "centroids", 1)
+
+	csvFile, err := os.Create(filename)
+	if err != nil {
+		log.Print("Failed creating file: ", err)
+		return false
+	}
+	defer csvFile.Close()
+
+	csvwriter := csv.NewWriter(csvFile)
+	for _, point := range res.Centroids {
+		line := strings.Fields(strings.Trim(fmt.Sprint(point.Values), "[]"))
+		csvwriter.Write(line)
+	}
+	csvwriter.Flush()
+	return true
+}
+
+func PrintResults(results []utils.Result) {
+	var mean time.Duration
+	// var averageTime int64
+	log.Print(color.HiGreenString("Test done!"))
+
+	for i, res := range results {
+		var executionTime string = res.ExecutionTime.String()
+		var iterations string = strconv.Itoa(res.Iterations)
+		log.Printf("[%s] results achieved in [%s] iterations with [%s] using [%s] mapper nodes",
+			color.HiWhiteString("Iteration #"+strconv.Itoa(i)),
+			color.YellowString(iterations), color.GreenString(executionTime), color.RedString(MAPPER_NUMS))
+		mean += res.ExecutionTime
+	}
+
+	//Print calculated centroids of only first run
+	log.Print("Results:")
+	for i, point := range results[0].Centroids {
+		line := strings.Fields(strings.Trim(fmt.Sprint(point.Values), "[]"))
+		log.Printf("RUN [#%d] ->  %s", i, strings.Join(line, "; "))
+	}
+
+	if len(results) > 1 {
+		mean = mean / time.Duration(len(results)) //len(results) = test runs
+		log.Print(color.HiCyanString("AVERAGE EXECUTION TIME: [%s]", mean))
+	}
+}
